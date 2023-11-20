609ca3eaf39c46515d32eaffda0fc67b995ef3ae
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-11-20 09:07:21 +0900

    fixed some bugs

--- girl.gd
+++ girl.gd
@@ -1,18 +1,16 @@
 extends CharacterBody2D
 
-export(int) var speed = 80.0
+@export var speed: int = 80.0;
 
 func _physics_process(delta):
 	var velocity = Vector2. ZERO
-	if input. is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1.0
-	if input. is_action_pressed("ui_left"):	
+	if Input.is_action_pressed("ui_left"):	
 		velocity.x -= 1.0
-	if input. is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1.0
-	if input. is_action_pressed("ui_up"):	
+	if Input.is_action_pressed("ui_up"):	
 		velocity.y -= 1.0
 		velocity = velocity.normalized()
-		move_and_slide(velocity * speed)
-		
-		onscreenkeyboard.set_on_screen_input_vector(velocity)
+	move_and_slide();
